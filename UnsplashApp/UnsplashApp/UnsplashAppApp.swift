727b1cce5976aa100cc5922837058cec6a079a0d
Nathael007 <nathaelmeunier@gmail.com>
2024-01-23 14:03:12 +0100

    UnsplashApp

---
+++ UnsplashApp/UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Nathael MEUNIER on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
